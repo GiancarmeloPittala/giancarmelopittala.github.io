792501a2bba22e051d4bfa53685cc5a34b708e4d
unknown <giancarmelo.pittala@gmail.com>
2023-04-22 15:39:01 +0200

    feat: change i18n option location

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -76,10 +76,37 @@ export default defineNuxtConfig({
   ],
 
   robots: {
-    sitemap: [ '/sitemap.xml' ],
+    sitemap: ['/sitemap.xml'],
   },
-  
-  i18n,
+
+  i18n: {
+    detectBrowserLanguage: {
+      useCookie: true,
+      cookieKey: 'i18n_redirected',
+      redirectOn: 'root',
+    },
+    locales: [
+      {
+        code: 'en',
+        iso: 'en-US',
+        file: 'en-US.json',
+        name: 'English',
+        icon: 'twemoji:flag-for-flag-united-states'
+      },
+      {
+        code: 'it',
+        iso: 'it-IT',
+        file: 'it-IT.json',
+        name: 'Italiano',
+        icon: 'openmoji:flag-italy'
+      },
+    ],
+    langDir: 'lang/',
+    defaultLocale: 'en',
+    lazy: true,
+    strategy: 'prefix_and_default',
+    vueI18n: './config/vueI18n.ts',
+  },
 
   schemaOrg: {
     host: DEFAULT_NUXT_PUBLIC_SITE_URL,
